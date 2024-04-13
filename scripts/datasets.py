1894b169247406055db558742d091ec5b59c2620
EmilHoejrup <emilhoejrup@gmail.com>
2024-04-13 10:42:11 +0200

    Try PE with downsampled data

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -214,7 +214,7 @@ class BNCI_LEFT_RIGHT(Dataset):
         self.X = self.X - baseline_mean
         # self.X = self.X[..., 768:1000]
         # self.X = self.X * self.hamming_window.view(1, 1, timepoints)
-        # self.X = F.avg_pool1d(self.X, 3, 2)
+        self.X = F.avg_pool1d(self.X, 3, 2)
         # self.X = F.max_pool1d(self.X, 3, 2)
         # self.X = self.X[..., ::3]
         # self.Y = self.Y.astype(float)
