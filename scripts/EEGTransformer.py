186262ed455cb4f90071acdf796d83da586f2bd3
EmilHoejrup <emilhoejrup@gmail.com>
2024-04-15 09:36:15 +0200

    Fix bug in ConformerCopy

--- scripts/EEGTransformer.py
+++ scripts/EEGTransformer.py
@@ -30,7 +30,7 @@ class EEGTransformer(nn.Module):
 class ConformerCopy(nn.Module):
     def __init__(self, vocab_size, nhead=2, num_classes=2, depth=2, emb_size=20, expansion=4, dropout=0.5):
         super(ConformerCopy, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, emb_size)
+        # self.embedding = nn.Embedding(vocab_size, emb_size)
         self.spatial_conv = nn.Conv2d(emb_size, emb_size, (22, 1), (1, 1))
         self.spatial_embedding = _SpatialEmbedding(emb_size, vocab_size)
         self.transformer_encoder = _TransformerEncoder(
@@ -40,17 +40,17 @@ class ConformerCopy(nn.Module):
     def forward(self, x):
         # x = torch.unsqueeze(x, dim=1)
         x = x.unsqueeze(dim=1)
-        print("Embedding shape: ", x.shape)
+        # print("Embedding shape: ", x.shape)
         # x = rearrange(x, 'b c t e -> b e c t')
         # print("Rearranged shape: ", x.shape)
         # x = self.spatial_conv(x)
         x = self.spatial_embedding(x)
-        print("Spatial conv shape: ", x.shape)
+        # print("Spatial conv shape: ", x.shape)
         x = x.squeeze(dim=2)
-        print("Squeezed shape: ", x.shape)
+        # print("Squeezed shape: ", x.shape)
         x = rearrange(x, 'b e t -> b t e')
         x = self.transformer_encoder(x)
-        print("Transformer shape: ", x.shape)
+        # print("Transformer shape: ", x.shape)
         # x, out = self.clshead(x)
         out = self.clshead(x)
         return out
