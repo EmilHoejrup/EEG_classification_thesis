e9a5e435087242f34f36d64a30f7e154dd7276f0
EmilHoejrup <emilhoejrup@gmail.com>
2024-04-29 19:10:52 +0200

    Major cleanup in dataset. Not sure it works yet

--- scripts/datasets.py
+++ scripts/datasets.py
@@ -36,26 +36,22 @@ def read_X_and_y(x_file, y_file):
     return X, Y
 
 
-# def load_bnci():
-#     samples_0 = torch.load(BNCI_DECONSTRUCTED / '0_samples.pth')
-#     samples_1 = torch.load(BNCI_DECONSTRUCTED / '1_samples.pth')
-#     samples_2 = torch.load(BNCI_DECONSTRUCTED / '2_samples.pth')
-#     samples_3 = torch.load(BNCI_DECONSTRUCTED / '3_samples.pth')
-#     labels_0 = torch.load(BNCI_DECONSTRUCTED / '0_labels.pth')
-#     labels_1 = torch.load(BNCI_DECONSTRUCTED / '1_labels.pth')
-#     labels_2 = torch.load(BNCI_DECONSTRUCTED / '2_labels.pth')
-#     labels_3 = torch.load(BNCI_DECONSTRUCTED / '3_labels.pth')
-#     X = torch.cat((samples_0, samples_1, samples_2, samples_3), dim=0)
-#     Y = torch.cat((labels_0, labels_1, labels_2, labels_3), dim=0)
-#     return X, Y
-
-
-class BNCI_LEFT_RIGHT_CONTINUOUS(Dataset):
-    def __init__(self, train=True):
+def fetch_data(data_dir):
+    if data_dir == BCI_IV_2A_DIR:
         if not BCI_IV_2A_DIR.exists():
             fetch_BCI_IV_2A()
+        X, Y = read_X_and_y(X_BCI_IV_2A, Y_BCI_IV_2A)
+    elif data_dir == BNCI2014_001_DIR:
+        if not BNCI2014_001_DIR.exists():
+            fetch_BNCI2014_001()
+        X, Y = read_X_and_y(X_BNCI2014_001, Y_BNCI2014_001)
+    return X, Y
+
+
+class CONTINUOUS_DATASET(Dataset):
+    def __init__(self, data_dir, train=True):
+        self.X, self.Y = fetch_data(data_dir)
         self.train = train
-        self.X, self.Y = read_X_and_y(X_BCI_IV_2A, Y_BCI_IV_2A)
 
         self.X = self.X.to(torch.float32)
         _, _, timepoints = self.X.shape
@@ -83,65 +79,30 @@ class BNCI_LEFT_RIGHT_CONTINUOUS(Dataset):
             return self.X_val[index], self.Y_val[index]
 
 
-# class BNCI_LEFT_RIGHT_CONTINUOUS(Dataset):
-#     def __init__(self, train=True):
-#         if not BNCI2014_001_DIR.exists():
-#             fetch_BNCI2014_001()
-#         self.train = train
-#         self.X, self.Y = read_X_and_y(X_BNCI2014_001, Y_BNCI2014_001)
-
-#         self.X = self.X.to(torch.float32)
-#         _, _, timepoints = self.X.shape
-#         baseline_start = 0
-#         baseline_end = 125
-#         baseline_mean = torch.mean(
-#             self.X[:, :, baseline_start:baseline_end], dim=2, keepdim=True)
-#         self.X = self.X - baseline_mean
-#         self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(
-#             self.X, self.Y, test_size=0.2, random_state=42)
-
-#     def get_X_shape(self):
-#         return self.X_train.shape
-
-#     def __len__(self):
-#         if self.train:
-#             return len(self.X_train)
-#         else:
-#             return len(self.X_val)
-
-#     def __getitem__(self, index):
-#         if self.train:
-#             return self.X_train[index], self.Y_train[index]
-#         else:
-#             return self.X_val[index], self.Y_val[index]
+class BCI_2A_CONTINUOUS(CONTINUOUS_DATASET):
+    def __init__(self, train=True):
+        super().__init__(BCI_IV_2A_DIR, train)
 
 
-class BNCI_LEFT_RIGHT_NEW_PE(Dataset):
-    def __init__(self, window_size, stride, strategy, train=True, threshold=0.001, n_clusters=128):
-        if not BNCI2014_001_DIR.exists():
-            fetch_BNCI2014_001()
+class BCI_2B_CONTINUOUS(CONTINUOUS_DATASET):
+    def __init__(self, train=True):
+        super().__init__(BCI_IV_2B_DIR, train)
+
+
+class PERMUTATION_DATASET(Dataset):
+    def __init__(self, window_size, stride, data_dir, train=True, threshold=0.001, ):
+        self.X, self.Y = fetch_data(data_dir)
+
         self.train = train
-        self.X, self.Y = read_X_and_y(X_BNCI2014_001, Y_BNCI2014_001)
+
         self.window_size = window_size
         self.stride = stride
         self.threshold = threshold
-        self.n_clusters = n_clusters
 
         _, _, timepoints = self.X.shape
-        # self.X, self.Y = self.X[:50], self.Y[:50]
-        # self.hamming_window = torch.hamming_window(timepoints)
-        # baseline_start = 0
-        # baseline_end = 125
-        # baseline_mean = torch.mean(
-        #     self.X[:, :, baseline_start:baseline_end], dim=2, keepdim=True)
-        # self.X = self.X - baseline_mean
-        # self.X = self.X[..., 768:1000]
-        # self.X = self.X * self.hamming_window.view(1, 1, timepoints)
-        # self.X = F.avg_pool1d(self.X, 3, 2)
+
         self.X = F.max_pool1d(self.X, 3, 3)
-        # self.X = self.X[..., ::3]
-        # self.Y = self.Y.astype(float)
-        # self.Y = self.Y.to(torch.float32)
+
         self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(
             self.X, self.Y, test_size=0.2, random_state=43)
         if self.train:
@@ -178,14 +139,6 @@ class BNCI_LEFT_RIGHT_NEW_PE(Dataset):
         X = X.tolist()
         new_sequence = []
 
-        # X[0] = 0
-        # for i in range(1, len(X)):
-        #     if X[i] > X[i-1]:
-        #         X[i] = 1
-        #     else:
-        #         X[i] = 0
-        # print(X)
-
         for i in range(0, len(X) - self.window_size, self.stride):
             window = X[i:(i+self.window_size)]
             sorted_window = sorted(window)
@@ -210,32 +163,26 @@ class BNCI_LEFT_RIGHT_NEW_PE(Dataset):
             return self.X_val[index], self.Y_val[index]
 
 
-class BNCI_LEFT_RIGHT(Dataset):
-    def __init__(self, window_size, stride, strategy, train=True, threshold=0.001, n_clusters=128):
-        if not BNCI2014_001_DIR.exists():
-            fetch_BNCI2014_001()
+class BCI_IV_2A_PERMUTED(PERMUTATION_DATASET):
+    def __init__(self, window_size, stride, train=True, threshold=0.001, ):
+        super().__init__(window_size, stride, BCI_IV_2A_DIR, train, threshold)
+
+
+class BCI_IV_2B_PERMUTED(PERMUTATION_DATASET):
+    def __init__(self, window_size, stride, train=True, threshold=0.001, ):
+        super().__init__(window_size, stride, BCI_IV_2B_DIR, train, threshold)
+
+
+class SIMPLE_PERMUTATION_DATASET(Dataset):
+    def __init__(self, window_size, stride, data_dir, train=True, threshold=0.001, ):
+        self.X, self.Y = fetch_data(data_dir)
         self.train = train
-        self.X, self.Y = read_X_and_y(X_BNCI2014_001, Y_BNCI2014_001)
         self.window_size = window_size
         self.stride = stride
         self.threshold = threshold
-        self.n_clusters = n_clusters
 
-        _, _, timepoints = self.X.shape
-        # self.X, self.Y = self.X[:50], self.Y[:50]
-        self.hamming_window = torch.hamming_window(timepoints)
-        baseline_start = 0
-        baseline_end = 125
-        # baseline_mean = torch.mean(
-        #     self.X[:, :, baseline_start:baseline_end], dim=2, keepdim=True)
-        # self.X = self.X - baseline_mean
-        # self.X = self.X[..., 768:1000]
-        # self.X = self.X * self.hamming_window.view(1, 1, timepoints)
-        # self.X = F.avg_pool1d(self.X, 3, 2)
         self.X = F.max_pool1d(self.X, 3, 3)
-        # self.X = self.X[..., ::3]
-        # self.Y = self.Y.astype(float)
-        # self.Y = self.Y.to(torch.float32)
+
         self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(
             self.X, self.Y, test_size=0.2, random_state=43)
         if self.train:
@@ -298,127 +245,11 @@ class BNCI_LEFT_RIGHT(Dataset):
             return self.X_val[index], self.Y_val[index]
 
 
-class BNCI_LEFT_RIGHT_COMPRESSED(Dataset):
-    def __init__(self, window_size, stride, strategy, train=True, threshold=0.001, n_clusters=128):
-        if not BNCI2014_001_DIR.exists():
-            fetch_BNCI2014_001()
-        self.train = train
-        self.X, self.Y = read_X_and_y(X_BNCI2014_001, Y_BNCI2014_001)
-        self.window_size = window_size
-        self.stride = stride
-        self.threshold = threshold
-        self.n_clusters = n_clusters
-
-        _, _, timepoints = self.X.shape
-        # self.X, self.Y = self.X[:50], self.Y[:50]
-        self.hamming_window = torch.hamming_window(timepoints)
-        baseline_start = 0
-        baseline_end = 125
-        baseline_mean = torch.mean(
-            self.X[:, :, baseline_start:baseline_end], dim=2, keepdim=True)
-        self.X = self.X - baseline_mean
-        # self.X = self.X[..., 768:1000]
-        # self.X = self.X * self.hamming_window.view(1, 1, timepoints)
-        self.X = F.avg_pool1d(self.X, 3, 2)
-        # self.X = F.max_pool1d(self.X, 3, 2)
-        # self.X = self.X[..., ::3]
-        # self.Y = self.Y.astype(float)
-        # self.Y = self.Y.to(torch.float32)
-        self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(
-            self.X, self.Y, test_size=0.2, random_state=43)
-        if self.train:
-            self.X_train = self._compress(self.X_train)
-            # self.X_train = self.X_train.to(torch.float32)
-        else:
-            self.X_val = self._compress(self.X_val)
-            # self.X_val = self.X_val.to(torch.float32)
-
-    def get_X_shape(self):
-        return self.X_train.shape
-
-    def get_vocab_size(self):
-        return len(self.permutations)
-
-    def _compress(self, X):
-        images, channels, timepoints = X.shape
-        X = torch.mean(X, dim=1, keepdim=True)
-        X = self._discretize(X)
-        return X.squeeze(1)
-
-    def _discretize(self, X):
-
-        values = [0, 1]
-        self.permutations = [list(perm)
-                             for perm in product(values, repeat=self.window_size)]
-        images, channels, timepoints = X.shape
-        discretized_X = []
-        for image in range(images):
-            image_sequence = []
-            for channel in range(channels):
-                sequence = X[image, channel]
-                discretized_sequence = self._permute(sequence)
-                image_sequence.append(discretized_sequence)
-            discretized_X.append(image_sequence)
-        return torch.tensor(discretized_X, dtype=torch.long)
-
-    def _permute(self, X):
-        X = X.tolist()
-        new_sequence = []
-        X[0] = 0
-        for i in range(1, len(X)):
-            if X[i] > X[i-1]:
-                X[i] = 1
-            else:
-                X[i] = 0
-        # print(X)
-
-        for i in range(0, len(X) - self.window_size, self.stride):
-            window = X[i:(i+self.window_size)]
-            new_sequence.append(self.permutations.index(window))
-        new_sequence.append(0)
-
-        return new_sequence
-
-    def __len__(self):
-        if self.train:
-            return len(self.X_train)
-        else:
-            return len(self.X_val)
-
-    def __getitem__(self, index):
-        if self.train:
-            return self.X_train[index], self.Y_train[index]
-        else:
-            return self.X_val[index], self.Y_val[index]
-# class BNCI_4_CLASS(Dataset):
-#     def __init__(self):
-#         if not BNCI_4_CLASS_DIR.exists():
-#             fetch_preprocess_and_save_BNCI2014_001_4_classes()
-#         self.dataset = load_concat_dataset(
-#             BNCI_4_CLASS_DIR, preload=False, target_name=None)
-
-#     def get_train_and_test_data(self):
-#         splitted = self.dataset.split('session')
-#         return splitted['0train'], splitted['1test']
-
-#     def __getitem__(self, index):
-#         return self.dataset[index]
+class SIMPLE_BCI_IV_2A_PERMUTED(SIMPLE_PERMUTATION_DATASET):
+    def __init__(self, window_size, stride, train=True, threshold=0.001, ):
+        super().__init__(window_size, stride, BCI_IV_2A_DIR, train, threshold)
 
 
-# def deconstruct_moabb_dataset(dataset, path):
-#     class_samples = {}
-#     for sample, label, _ in dataset:
-#         if label not in class_samples:
-#             class_samples[label] = []
-#         class_samples[label].append((sample, label))
-#     path.mkdir(parents=True, exist_ok=True)
-#     for label, sample in class_samples.items():
-#         samples, labels = zip(*sample)
-#         samples = torch.tensor(samples)
-#         labels = torch.tensor(labels)
-#         sample_path = path / f"{label}_samples.pth"
-#         torch.save(samples, sample_path)
-#         label_path = path / f"{label}_labels.pth"
-#         torch.save(labels, label_path)
-
-##################### OLD DATASETS #####################
+class SIMPLE_BCI_IV_2B_PERMUTED(SIMPLE_PERMUTATION_DATASET):
+    def __init__(self, window_size, stride, train=True, threshold=0.001, ):
+        super().__init__(window_size, stride, BCI_IV_2B_DIR, train, threshold)
--- scripts/trainer.py
+++ scripts/trainer.py
@@ -8,121 +8,6 @@ from torch.optim.lr_scheduler import LRScheduler
 from sklearn.metrics import precision_score, recall_score
 
 
-# class Trainer(nn.Module):
-#     def __init__(self, model: torch.nn.Module,
-#                  train_loader: torch.utils.data.DataLoader,
-#                  val_loader: torch.utils.data.DataLoader,
-#                  loss_fun: torch.nn.Module,
-#                  optimizer: torch.optim.Optimizer,
-#                  scheduler: LRScheduler,
-#                  device: torch.device,
-#                  wandb_logging=True):
-
-#         super().__init__()
-#         self.model = model
-#         self.train_loader = train_loader
-#         self.val_loader = val_loader
-#         self.loss_fun = loss_fun
-#         self.optimizer = optimizer
-#         self.device = device
-#         self.train_losses = []
-#         self.train_accuracies = []
-#         self.val_losses = []
-#         self.val_accuracies = []
-#         self.scheduler = scheduler
-#         self.model.to(device)
-#         self.wandb_logging = wandb_logging
-#         if wandb_logging:
-#             wandb.watch(self.model, self.loss_fun, log='all')
-
-#     def fit(self, epochs=5, print_metrics=True):
-#         for _ in tqdm(range(epochs)):
-#             self._train_step(print_metrics)
-#             self._val_step(print_metrics)
-
-#     def get_metrics(self):
-#         return self.train_losses, self.train_accuracies, self.val_losses, self.val_accuracies
-
-#     def plot_train_val_scores(self):
-#         with torch.inference_mode():
-#             plot_train_val_scores(
-#                 self.train_losses, self.train_accuracies, self.val_losses, self.val_accuracies)
-
-#     def _train_step(self, print_metrics):
-#         train_loss, train_acc = 0, 0
-#         self.model.train()
-#         for batch, (X, y) in enumerate(self.train_loader):
-#             X, y = X.to(self.device), y.to(self.device)
-#             y_logits = self.model(X)
-
-#             # Calculate metrics
-#             loss = self.loss_fun(y_logits, y)
-#             train_loss += loss.item()
-#             train_acc += (y_logits.argmax(1) == y).sum().item()
-#             # Backpropagation
-#             self.optimizer.zero_grad()
-#             loss.backward()
-#             self.optimizer.step()
-
-#         self.scheduler.step()
-#         train_loss /= len(self.train_loader)
-#         train_acc /= (len(self.train_loader.dataset))
-
-#         self.train_losses.append(train_loss)
-#         self.train_accuracies.append(train_acc)
-
-#         if (print_metrics):
-#             print(
-#                 f"Train loss: {train_loss:.5f} | Train acc: {train_acc*100:.5f}")
-#         if self.wandb_logging:
-#             wandb.log({"Train Loss": train_loss, "Train Accuracy": train_acc})
-
-#     def _val_step(self, print_metrics):
-#         val_loss, val_acc = 0, 0
-#         val_precision, val_recall = 0, 0
-#         self.model.eval()
-#         with torch.inference_mode():
-#             y_true, y_pred = [], []
-#             for batch, (X, y) in enumerate(self.val_loader):
-#                 X, y = X.to(self.device), y.to(self.device)
-#                 y_logits = self.model(X)
-#                 val_acc += (y_logits.argmax(1) ==
-#                             y).type(torch.float).sum().item()
-
-#                 # Calculate metrics
-#                 val_loss += self.loss_fun(y_logits, y)
-#                 # Go from logits -> prediction labels
-#                 y_pred.extend(y_logits.argmax(1).cpu().numpy())
-#                 y_true.extend(y.cpu().numpy())
-
-#             # Calculate test loss and accuracy average per batch
-#             val_loss /= len(self.val_loader)
-#             val_acc /= (len(self.val_loader.dataset))
-#             # self.log('validation loss', val_loss)
-#             # self.log('validation accuracy', val_acc)
-#             self.val_losses.append(val_loss)
-#             self.val_accuracies.append(val_acc)
-
-#         val_precision = precision_score(y_true,
-#                                         y_pred, average='binary', zero_division=0)
-#         val_recall = recall_score(y_true, y_pred, average='binary',
-#                                   zero_division=0)
-#         if (print_metrics):
-#             print(f"Val   loss: {val_loss:.5f} | Val   acc: {val_acc*100:.5f}")
-#             print(
-#                 f"Val Precision: {val_precision:.5f} | Val Recall: {val_recall:.5f}")
-#             print(f"y_true: {y_true}")
-#             print(f"y_pred: {y_pred}")
-
-#         if self.wandb_logging:
-#             wandb.log({"Validation Loss": val_loss,
-#                        "Validation Accuracy": val_acc,
-#                        "Validation Precision": val_precision,
-#                        "Validation Recall": val_recall,
-#                        "conf_mat": wandb.plot.confusion_matrix(probs=None, y_true=y_true, preds=y_pred)})
-from sklearn.metrics import precision_score, recall_score, accuracy_score
-
-
 class Trainer(nn.Module):
     def __init__(self, model, train_loader, val_loader, loss_fun, optimizer, scheduler, device, wandb_logging=True):
 
@@ -218,4 +103,6 @@ class Trainer(nn.Module):
             wandb.log({"Validation Loss": val_loss,
                        "Validation Accuracy": val_acc,
                        "Validation Precision": val_precision,
-                       "Validation Recall": val_recall})
+                       "Validation Recall": val_recall,
+                       "conf_mat": wandb.plot.confusion_matrix(probs=None, y_true=y_true, preds=y_pred)
+                       })
